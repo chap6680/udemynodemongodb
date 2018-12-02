15bb83baf452f83b9c4056cf607834808feab031
david chapman <chap6680@gmail.com>
2018-12-02 16:52:18 -0700

    mongoose post

--- server/server.js
+++ server/server.js
@@ -1,30 +1,30 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoMongooseApp');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-	text: {
-type: String
-	},
-	completed: {
-type: Boolean
-	},
-	completedAt: {
-type: Number
-	}
-});
+var app = express();
+app.use(bodyParser.json());
 
-var newTODO = new Todo({
-	text: 'walk cat',
-	completed: true,
-	completedAt: 12
-});
-
-newTODO.save().then((doc) => {
-	console.log(JSON.stringify(doc, undefined, 2));
-	mongoose.connection.close();
-}, (err) => {
-	console.log('unable to save', err);
+app.post('/todos', (req, res) => { 
+	console.log(req.body);
+	var todo = new Todo({
+		text: req.body.text
 	});
 
+	console.log('going into save');
+
+	todo.save().then((doc) => {
+		console.log('inside save');
+		res.send(doc);
+	}, (err) => {
+		console.log('error: ');
+		res.status(400).send(err);
+	});
+});
+
+app.listen(3000, () => { 
+	console.log('Server started on 3000');
+})
