c38540195d3ab7536fe38af9109b802166f08df9
david chapman <chap6680@gmail.com>
2018-12-02 20:33:55 -0700

    test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoMongooseApp');
+mongoose.connect('mongodb://localhost:27017/TodoMongooseApp', {useNewUrlParser: true});
 
 module.exports = { mongoose };
 
--- server/server.js
+++ server/server.js
@@ -25,6 +25,8 @@ app.post('/todos', (req, res) => {
 	});
 });
 
-app.listen(3000, () => { 
+app.listen(3000, () => {
 	console.log('Server started on 3000');
-})
+});
+
+module.exports = { app };
