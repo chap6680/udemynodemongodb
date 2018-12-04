ec139fd7dc5b2dc05c224f48af530b6967257329
david chapman <chap6680@gmail.com>
2018-12-04 06:53:24 -0700

    get todos via api

--- server/server.js
+++ server/server.js
@@ -4,9 +4,35 @@ var bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+const {ObjectID} = require('mongodb');
+
 
 var app = express();
 app.use(bodyParser.json());
+
+app.get('/todos/:id', (req, res) => {
+	console.log('start');
+	var id = req.params.id;
+	console.log(id);
+	//	res.send(req.parms);
+	
+	//validateid
+	if (!ObjectID.isValid(id)) {
+		console.log('checkvalid');
+		return res.status(404).send();
+	};
+	
+	Todo.findById(id).then((todo) => {
+		console.log('find function');
+		if (!todo) {
+			return res.status(404).send();
+		}
+		res.send({ todo });
+		//console.log('todos by id:', todo);
+	}).catch((e) => {
+		return res.status(400).send();
+	});
+});
 
 app.post('/todos', (req, res) => { 
 	console.log(req.body);
@@ -24,6 +50,7 @@ app.post('/todos', (req, res) => {
 		res.status(400).send(err);
 	});
 });
+
 
 app.get('/todos', (req, res) => { 
 	Todo.find().then((todos) => { 
