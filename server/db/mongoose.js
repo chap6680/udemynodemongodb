73a3b82b074a6e35a894e5829c8954d6acd75190
david chapman <chap6680@gmail.com>
2018-12-11 01:54:16 -0700

    update X

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODDB_URI||'mongodb://localhost:27017/TodoMongooseApp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/TodoMongooseApp', {useNewUrlParser: true});
 
 module.exports = { mongoose };
 
